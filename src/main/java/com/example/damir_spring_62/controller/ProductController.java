71d5022bfe0a43f8f919bac73fc4f66a9e2d8ef5
Damir Kadyrzhanov <damirk120404@gmail.com>
2024-06-30 14:01:23 +0500

    filtration

--- src/main/java/com/example/damir_spring_62/controller/ProductController.java
+++ src/main/java/com/example/damir_spring_62/controller/ProductController.java
@@ -3,11 +3,13 @@ package com.example.damir_spring_62.controller;
 import com.example.damir_spring_62.pojos.Product;
 import com.example.damir_spring_62.services.ProductBean;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController
+@Controller
 @RequestMapping(path = "/products_task")
 public class ProductController {
     private final ProductBean productBean;
@@ -36,4 +38,21 @@ public class ProductController {
         }
         return "Не хватает полей для нового товара!";
     }
+
+    @GetMapping(path = "/products")
+    public String getProducts(Model model){
+        String category = "";
+        model.addAttribute("products", productBean.getProducts());
+        return "view_page_1";
+    }
+
+    @GetMapping(path = "/products/filter")
+    public String filter(@RequestParam(name = "category") String categoryName, Model model){
+        List<Product> temp = productBean.getAllProducts().stream()
+                .filter(product -> product.getCategory().contains(categoryName)).toList();
+        model.addAttribute("products", temp);
+        model.addAttribute("category", categoryName);
+        return "view_page_1";
+    }
+
 }
